238ed5fee1dc8aa9905866333ea019c4c467bc2d
ayush-ojha-11 <ayushojha1512@gmail.com>
2024-12-18 13:11:41 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("Welcome to server!");
-});
-
 app.listen(7099, () => {
   console.log("Server is successfully running on port 7099");
+});
+
+app.use("/ab+c", (req, res) => {
+  res.send("Test route...");
+});
+
+app.use("/home", (req, res) => {
+  res.send("Home route...");
+});
+
+app.get("/user", (req, res) => {
+  res.send({ "first name": "Ayush" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully!!");
 });
