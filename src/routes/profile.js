a95b1d04bf4d29991f0fff656b69854d857604f6
ayush-ojha-11 <ayushojha1512@gmail.com>
2024-12-23 17:18:32 +0530

    changes to a lot of APIs

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
-
+const bcrypt = require("bcrypt");
 const profileRouter = express.Router();
 
 //Profile view API
@@ -30,6 +30,35 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
       message: `${loggedInUser.firstName}, your profile was updated successfully.`,
       data: loggedInUser,
     });
+  } catch (error) {
+    res.status(400).send("Error: " + error.message);
+  }
+});
+
+profileRouter.patch("/profile/changePassword", userAuth, async (req, res) => {
+  try {
+    // take current password and check if it is valid
+    const loggedInUser = req.user;
+    const currentPassword = req.body.currentPassword;
+    const isPasswordValid = await loggedInUser.validatePassword(
+      currentPassword
+    );
+    if (!isPasswordValid) {
+      throw new Error("Wrong Password!");
+    }
+
+    // if the current password is valid take the new password
+    // and encrypt it before saving in the database
+
+    const newPassword = req.body.newPassword;
+    const newPasswordHash = await bcrypt.hash(newPassword, 10);
+    loggedInUser["password"] = newPasswordHash;
+    await loggedInUser.save();
+
+    // logout the user
+    res.cookie("token", null, { expires: new Date(Date.now()) });
+
+    res.send("Password changed successfully");
   } catch (error) {
     res.status(400).send("Error: " + error.message);
   }
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,14 +1,59 @@
 const express = require("express");
 const User = require("../models/user");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
 const requestRouter = express.Router();
 
 //sendConnectionRequest API
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  res.send(user.firstName + " sent the connection request.");
-});
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      // allow only ignored and interested
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        throw new Error("Invalid Status type!");
+      }
+
+      // check if already a request exits or not
+
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          {
+            fromUserId: toUserId,
+            toUserId: fromUserId,
+          },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        throw new Error("Request already exists!");
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const connectionData = await connectionRequest.save();
+
+      res.json({
+        message: "Connection request sent successfully!",
+        connectionData,
+      });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+);
 
 module.exports = requestRouter;
