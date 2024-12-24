b1848ded59cafb68983c11439e1a564083c6d4e9
ayush-ojha-11 <ayushojha1512@gmail.com>
2024-12-24 13:15:57 +0530

    user/connections API

--- src/routes/user.js
+++ src/routes/user.js
@@ -34,11 +34,20 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId", USER_SAFE_DATA);
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
 
     res.json({
       message: "Connection data retrieved successfully.",
-      data: connectionRequests,
+      data: data,
     });
   } catch (error) {
     res.status(400).send({ message: error.message });
